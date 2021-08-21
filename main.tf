954515e720f9802a2536d7dcfcd46acd6e298c05
toxeek <avernopolis@gmail.com>
2021-08-21 20:14:27 +0200

    adding vpc module, thx Shirwa

--- main.tf
+++ main.tf
@@ -54,3 +54,30 @@ locals {
     }
   ]
 }
+
+module "vpc" {
+  source                      = "terraform-aws-modules/vpc/aws"
+  version                     = "~> 2.0"
+  name                        = "${terraform.workspace}-${var.name_prefix}-vpc"
+  cidr                        = var.vpc_cidr
+  azs                         = var.azs
+  private_subnets             = var.private_subnets_cidrs
+  public_subnets              = var.public_subnets_cidrs
+  database_subnets            = var.db_subnets_cidrs
+  enable_nat_gateway          = true
+  single_nat_gateway          = true
+
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                                                           = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"                                                  = "1"
+  }
+}
