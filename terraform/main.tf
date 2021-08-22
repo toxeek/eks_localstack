4d5a6bfeeef09f0a1a8e76ed3460d18cd63115a9
toxeek <avernopolis@gmail.com>
2021-08-22 17:58:26 +0200

    adding helm provider and data sources

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,25 @@ provider "kubernetes" {
   load_config_file            = false
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+  version = "~> 1.2"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+
+
 # locals, developer and admin users groups for eks cluster
 locals {
   admin_user_map_users = [
