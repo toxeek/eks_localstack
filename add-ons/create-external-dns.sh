6663699e6d112daa160ad5d79a2a577968a42326
toxeek <avernopolis@gmail.com>
2021-08-23 20:12:17 +0200

    adding extdns addon

--- add-ons/create-external-dns.sh
+++ add-ons/create-external-dns.sh
@@ -8,8 +8,8 @@ helm install external-dns \
   --set provider=aws \
   --set aws.zoneType=public \
   --set txtOwnerId=external-dns \
-  --set domainFilters[0]=toxeeklabs.com \
+  --set domainFilters[0]=$1 \
   --set policy=sync \
-  --set rbac.serviceAccountAnnotations."eks\.amazonaws\.com/role-arn=arn:aws:iam::$1:role/ExternalDnsIAMRole" \
+  --set rbac.serviceAccountAnnotations."eks\.amazonaws\.com/role-arn=arn:aws:iam::$2:role/ExternalDnsIAMRole" \
   --namespace kube-system \
   stable/external-dns
