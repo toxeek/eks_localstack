054c5babf3b1f7be377ad48f659f695e73ef127d
toxeek <avernopolis@gmail.com>
2021-08-23 20:11:04 +0200

    adding subnets vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -77,3 +77,35 @@ variable "domain" {
     description = "domain to use with external dns"
     type = "string"
 }
+
+// VPC
+
+variable "region" {
+  description = "The AWS region where the infraestructure will be located"
+  default     = "eu-west-2"
+  type        = string
+}
+
+variable "azs" {
+  description = "The availability zones where the infraestructure will be located"
+  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  type        = list(string)
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the cloud VPC"
+  default     = "172.16.0.0/16"
+  type        = string
+}
+
+variable "private_subnets_cidrs" {
+  description = "The CIDRs of private subnets of the cloud VPC. The default values allow 4094 hosts per subnet"
+  default     = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  type        = list(string)
+}
+
+variable "public_subnets_cidrs" {
+  description = "The CIDRs of public subnets of the cloud VPC. The default values allow 4094 hosts per subnet"
+  default     = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  type        = list(string)
+}
